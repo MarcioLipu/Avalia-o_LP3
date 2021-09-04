accc2b8c9793bb04e610c3217347aec4d7923f4d
MarcioLipu <marciolipu@gmail.com>
2021-09-04 14:14:45 -0400

    Commit de LP3

--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -1,48 +1,53 @@
-import {Client} from "../models/client";
+import { Client } from "../models/Client";
 
-export class clientController{
+export class ClientController {
+
     /**
-     * Isto é o nosso banco de dados !
-     */private clients: Client[]
+     * Isto é o nosso banco de dados!
+     */
+    private _clients: Client[]
 
-     constructor(){
-         this.clients=[]
-     }
-     save(client: Client): boolean{
-         const clientFound=
-         this.clients.find(c=> c.id==client.id)
-         /**
-          * Se não encontrou  nenhum  cliente com o
-          * mesmo id  do cliente recebido ,salva o
-          * novo cliente  no "banco de dados"
-          */
-         if(!clientFound){
-             this.clients.push(this.client)
-             return true
-         }
-         return false
-     }
-     update(client: Client):boolean{
-         for(let i=0; i< this._clients.length;i++){
-             if(client.id==this._clients[i].id){
-                 this.clients[i]=client
-                 return  true
-             }
-         }
-         return false
-     }
-     delete(id:number):boolean{
-         const length1=this._client.length
-         this.clients = this._clients._filter(c=> c.id !=id)
-         const length2 =this._clients.length
-         return length1 < length1
-     }
+    constructor() {
+        this._clients = []
+    }
 
-     findAll():Client[]{
-         return this.clients
-     }
+    save(client: Client): boolean {
+        const clientFound =
+            this._clients.find(c => c.id == client.id)
 
-    
-     
-     }
+        /**
+         * Se não encontrou nenhum cliente com o
+         * mesmo id do cliente recebido, salva o
+         * novo cliente no "banco de dados"
+         */
+        if (!clientFound) {
+            this._clients.push(client)
+            return true
+        }
+
+        return false
+    }
+
+    update(client: Client): boolean {
+        for (let i = 0; i < this._clients.length; i++) {
+            if (client.id == this._clients[i].id) {
+                this._clients[i] = client
+                return true
+            }
+        }
+
+        return false
+    }
+
+    delete(id: number): boolean {
+        const length1 = this._clients.length
+        this._clients = this._clients.filter(c => c.id != id)
+        const length2 = this._clients.length
+        return length2 < length1
+    }
+
+    findAll(): Client[] {
+        return this._clients
+    }
+}
 
--- src/routes/clients.ts
+++ src/routes/clients.ts
@@ -1,70 +1,39 @@
-import { Request, response, Router } from "express"
-import { clientController } from "../controllers/clientController"
-import { Client } from "../models/client"
+import { Request, Response, Router } from 'express'
+import { ClientController } from '../controllers/ClientController'
+import { Client } from '../models/Client'
 
 export const clientsRouter = Router()
-const clientCtrl = new clientController()
+const clientCtrl = new ClientController()
 let idCounter = 1
 
-clientsRouter.post('/',(req: Request, res:  Response)=>{
-    /**
-     * const name =req.body.name
-     * const phone=req.body.phone
-    */
+clientsRouter.post('/', (req: Request, res: Response) => {
+    /*const name = req.body.name
+    const phone = req.body.phone*/
 
     /**
-     * associação por desestruturação 
+     * Associação por desestruturação
      */
-    const {name, phone} = req.body
+    const { name, phone } = req.body
     const client = new Client(idCounter, name, phone)
-    if(clientCtrl.save(client)){
+    if (clientCtrl.save(client)) {
         idCounter++
         /**
-         * 201 -created
+         * 201 - Created
          */
-
         return res.status(201).json({
             message: 'Client created'
         })
-    }else}
-    /**
-     * 409 -conflict
-     */
-    return res.status(409).json(
-        {
-            message: 'A client  with this  id  already exist'
-        }
-     })
-
+    } else {
+        /**
+         * 409 - Conflict
+         */
+        return res.status(409).json({
+            message: 'A client with this id already exists'
+        })
     }
+
 })
-clientsRouter.get('/', ('req: Request, res: Response') =>{
-    return res.json({clients:clientCtrl.findAll})
-}
-)
 
-if(clientCtrl.save(client)){
-    idCounter++
-    /**
-     * 202 -created
-     */
-
-    return res.status(202).json({
-        message: 'Client created'
-    })
-}else}
-/**
- * 404 -conflict
- */
-return res.status(204).json(
-    {
-        message: 'A client  with this  id  already exist'
-    }
- })
-
-}
+clientsRouter.get('/', (req: Request, res: Response) => {
+    return res.json({ clients: clientCtrl.findAll() })
 })
-clientsRouter.get('/', ('req: Request, res: Response') =>{
-return res.json({clients:clientCtrl.findAll})
-}
-)
